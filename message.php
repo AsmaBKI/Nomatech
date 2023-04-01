e95c92d5799cba0465ecb860d5a55e965acfd7dc
ANDY PHILETAS <115489301+AndyPHILETAS@users.noreply.github.com>
2023-04-01 18:00:43 +0200

    Add files via upload
    css messagerie

--- message.php
+++ message.php
@@ -2,8 +2,7 @@
 //connexion session
 session_start();
 //connexion base de donnees
-require_once "./admin/databaseNomaTech.php"; // connection à la base de donnée
-// au lieu d'écrire la phrase  : $db = new PDO("mysql:host=localhost;dbname=nomatech","root","root");
+require_once "./admin/databaseNomaTech.php";
 //securité (si l'utililisateur n'est pas connecté ou n'a pas de compte)
 if(!$_SESSION['email'] AND !$_SESSION['mdp']){ 
 
@@ -46,118 +45,30 @@ if(!$_SESSION['email'] AND !$_SESSION['mdp']){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="style.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Alkatra:wght@500;700&family=Open+Sans:wght@300;500&family=Oswald:wght@200&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600&display=swap" rel="stylesheet">
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css"
-    />
-
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
-      crossorigin="anonymous"
-    />
-    <title>Noma'Tech</title>
-  </head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+    rel="stylesheet" 
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
+    crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+    <title>Messages</title>
+</head>
 <body>
-
-    <!------------------Navigation Central------------------>
-    <header class="nav-central">
-      <div class="Bloc-container1">
-        <div class="logo-nav">
-          <span><a href="./testaccueil.php" style="font-family: 'Alkatra', serif; font-size: 40px; color: #240085; text-decoration:none">Noma'tech</a></span>
-        </div>
-      </div>
-
-      <div class="bloc-container2">
-        <a href="./consultationProjet.php">Jobs</a>
-
-        <a href="./index.php">Talents</a>
-
-        <a href="./connexion.php">Se connecter</a>
-
-        <button class="inscription" onclick="window.location.href = './inscrip2803.php';">
-          S'inscrire
-        </button>
-      </div>
-    </header>
-    <!------------------ Menu Burger--------------------->
-    <div class="bouton-rond">
-      <div class="cont-ligne">
-        <div class="ligne"></div>
-      </div>
-    </div>
-
-    <!------------------Navigation Gauche------------------>
-
-    <nav class="nav-gauche">
-      <div class="bloc-menu-1">
-        <div class="logo">
-          <span class="ml-2"><a href="./testaccueil.php" style="font-family: 'Alkatra', serif; font-size: 30px; color: #240085">Noma'tech</a></span>
-        </div>
-      </div>
-      <div class="bloc-menu">
-        <span class="nav-menu-item">
-          <a href="./consultationProjet.php">Jobs</a>
-        </span>
-      </div>
-      <div class="bloc-menu">
-        <span class="nav-menu-item">
-          <a href="./index.php">Talents</a>
-        </span>
-      </div>
-      <div class="bloc-menu">
-        <span class="nav-menu-item">
-          <a href="./connexion.php">Se connecter</a>
-        </span>
-      </div>
-      <div class="bloc-menu">
-        <span class="nav-menu-item">
-          <a href="./inscrip2803.php">S'inscrire gratuitement</a>
-        </span>
-      </div>
-      <div class="bloc-menu">
-        <span class="nav-menu-item">
-          <a href="./team.php">A propos de</a>
-        </span>
-      </div>
-      <div class="bloc-menu">
-        <span class="nav-menu-item">
-          <a href="#">Contact</a>
-        </span>
-      </div>
-      <div class="bloc-menu">
-        <div class="logo-contact">
-          <a href="#"
-            ><img src="./img/Mail contact.png" alt="Logo Contact"
-          /></a>
-        </div>
-      </div>
-    </nav>
 <!-- creation d'un formulaire pour les messages . -->
 <form method="POST" action=" " align="center" >
 
-    <div class="col-md-12 mt-4">
-        <textarea class="form-label" name="messages"  cols="30" rows="10" placeholder="Ecrivez votre message" ></textarea>
+    <div class="container-msg">
+        <div class="txt-msg">
+            <textarea class="message-form" name="messages"  placeholder="Ecrivez votre message..."></textarea>
+        </div>
+        <div class="sub-msg">
+            <button type="submit" name ="envoi" class="" > <img src="./img/white-24dp/2x/outline_send_white_24dp.png" alt=""></button>
+        </div>
     </div>
 
-    <div div class="form-button mt-3 ml-3">
-        <button type="submit" name ="envoi" class="btn btn-primary" > Envoyer </button>
-    </div>
+
 
     <!-- afficher les messages que j'ai envoyer dans la section... 
  la boucle while récupère les messages en fonction de l'ID du 
@@ -172,11 +83,15 @@ if(!$_SESSION['email'] AND !$_SESSION['mdp']){
             while($messages = $recupMessages->fetch()){
                 if($messages['id_destinataire'] == $_SESSION['id_user']){
         ?>  
-                    <p style="color:red;"> <?= $messages['messages'];?> </p>
+        <div class="msg-in">
+            <p><?= $messages['messages'];?></p>
+        </div> 
         <?php 
                 }elseif($messages['id_destinataire'] == $getid){
         ?>  
-                    <p style="color:green;"> <?= $messages['messages'];?> </p>
+        <div class="msg-out">
+        <p> <?= $messages['messages'];?> </p>
+      </div> 
         <?php 
 
                 }   
@@ -184,43 +99,6 @@ if(!$_SESSION['email'] AND !$_SESSION['mdp']){
 
         ?>
     </section>
-
-        <!------------------ FOOTER ------------------>
-        <div class="footer-dark mt-5">
-            <footer>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-6 col-md-3 item">
-                            <h3>Service</h3>
-                            <ul>
-                                <li><a href="./consultationProjet.php">Jobs</a></li>
-                                <li><a href="./index.php">Talents</a></li>
-                                <li><a href="./connexion.php">Se connecter</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-sm-6 col-md-3 item">
-                            <h3>About</h3>
-                            <ul>
-                                <li><a href="./team.php">Team</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-sm-6 col-md-3 item text">
-                            <h3>Contact</h3>
-                            <p>Nomatech</p>
-                            <p>3 rue Armand Moisant</p>
-                            <p>75015 Paris</p>
-                            <p>01.55.65.53.57</p>
-                        </div>
-                        <div class="col item social">
-                            <a href="https://www.instagram.com/nomatech.fr/" target="_blank"><i class="icon ion-social-instagram"></i></a>
-                            <br><br>
-                            <a href="https://www.linkedin.com/in/noma-tech-81a019270/" target="_blank"><i class="icon ion-social-linkedin"></i></a>
-                        </div>
-                    </div>
-                    <p class="copyright">Noma'Tech © 2023</p>
-                </div>
-            </footer>
-        </div> 
-    </body>
-        <script src="./app.js"></script>
+    
+</body>
 </html>
